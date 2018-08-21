9d7e99ee87b699c1c6b334350098c4c30c6e0f94
Mariano <mmoretticom@gmail.com>
2018-08-20 23:01:11 -0300

    MVC

---
+++ Sistema/Models/Categorias.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sistema.Models
+{
+    public class Categorias
+    {
+        public int CategoriaID { get; set; }
+        [Required (ErrorMessage ="Campo Obligatorio!.")]
+        [StringLength(50,MinimumLength = 3,ErrorMessage ="El nombre ingresado es invalido.")]
+        public string Nombre { get; set; }
+        [StringLength(256, ErrorMessage ="La descripcion supera los 256 caracteres.")]
+        [Display(Name = "Descripción")]
+        public string Descripcion { get; set; }
+        public Boolean Estado { get; set; }
+    }
+}
